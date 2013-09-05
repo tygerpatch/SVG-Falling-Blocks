5e66e07e60fffbb90159caa138e4af7c4720d051
Todd Gerspacher <toddgerspacher27@gmail.com>
2013-09-05 18:15:15 -0400

    Square is no longer associated with a FallingBlocks Board.

--- Square.js
+++ Square.js
@@ -1,49 +1,48 @@
 // Each square has a color, size, and row, column position on board.
 function Square() {
-   this.position = new Position();
+   this.rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
 
-   this.setPosition = Square_setPosition;
-   this.getPosition = Square_getPosition;
+   this.rect.setAttribute("stroke", Color.BLACK);
+   this.rect.setAttribute("strokeWidth", "1");
 
-   this.color = Color.BLUE;
+   this.setXCoordinate = Square_setXCoordinate;
+   this.setYCoordinate = Square_setYCoordinate;
 
    this.getColor = Square_getColor;
    this.setColor = Square_setColor;
 
-   this.paint = Square_paint;
+   this.getSize = Square_getSize;
+   this.setSize = Square_setSize;
 
-   this.rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-
-   this.rect.setAttribute("width", Square.SIZE);
-   this.rect.setAttribute("height", Square.SIZE);
-   this.rect.setAttribute("stroke", Color.BLACK);
-   this.rect.setAttribute("strokeWidth", "1");
+   this.appendTo = Square_appendTo;
 }
 
-Square.SIZE = 20;
-
-// TODO: replace paint with attach(svg), detach(svg)
-function Square_paint(svg) {
-   svg.appendChild(this.rect);
+function Square_getSize() {
+   return this.rect.getAttribute("width");
 }
 
-function Square_setPosition(row, column) {
-   var x = row * Square.SIZE;
-   var y = column * Square.SIZE;
-
-   this.rect.setAttribute("x", x);
-   this.rect.setAttribute("y", y);
-}
-
-function Square_getPosition() {
-   return this.position;
-}
-
-function Square_getColor() {
-   return this.color;
+function Square_setSize(size) {
+   this.rect.setAttribute("width", size);
+   this.rect.setAttribute("height", size);
 }
 
 function Square_setColor(color) {
-   this.color = color;
-   this.rect.setAttribute("fill", this.color);
+   this.rect.setAttribute("fill", color);
+}
+
+function Square_getColor() {
+   return this.rect.getAttribute("fill");
+}
+
+// TODO: detachFrom(svg)
+function Square_appendTo(svg) {
+   svg.appendChild(this.rect);
+}
+
+function Square_setXCoordinate(x) {
+   this.rect.setAttribute("x", x);
+}
+
+function Square_setYCoordinate(y) {
+   this.rect.setAttribute("y", y);
 }
